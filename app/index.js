769a2e0aa8fa2a053d8205e1c67ac43cefc12744
newlinedeveloper <newlinedeveloper@gmail.com>
2023-08-05 22:59:32 +0530

    app version 2

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send({ message : "K8s deployment : Version 1"}))
+app.get('/', (req, res) => res.send({ message : "K8s deployment : Version 2"}))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
